4b11dfa4398d7a81cf0c2b4a7bd912d58809c8ef
Adetunji Dahunsi <tjdah100@gmail.com>
2024-12-27 16:30:08 +0300

    Add stubs for other screens in the app

---
+++ feature-messages/src/commonMain/kotlin/com/tunjid/heron/messages/State.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2024 Adetunji Dahunsi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tunjid.heron.messages
+
+import com.tunjid.heron.scaffold.navigation.NavigationAction
+import com.tunjid.heron.scaffold.navigation.NavigationMutation
+import com.tunjid.treenav.pop
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+
+
+@Serializable
+data class State(
+    @Transient
+    val messages: List<String> = emptyList(),
+)
+
+
+sealed class Action(val key: String) {
+
+    sealed class Navigate : Action(key = "Navigate"), NavigationAction {
+        data object Pop : Navigate() {
+            override val navigationMutation: NavigationMutation = {
+                navState.pop()
+            }
+        }
+    }
+}
