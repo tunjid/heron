9c58e31a84634eb7de00f8aae2e50fd11dc54b5c
Adetunji Dahunsi <tjdah100@gmail.com>
2024-12-27 08:56:26 +0300

    Use base64 to encode and decode urls in route query params

--- scaffold/src/androidMain/kotlin/com/tunjid/heron/scaffold/navigation/StringEncoding.android.kt
+++
@@ -1,7 +0,0 @@
-package com.tunjid.heron.scaffold.navigation
-
-import android.net.Uri
-
-internal actual fun String.encodeUrl(): String = Uri.encode(this)
-
-internal actual fun String.decodeUrl(): String = Uri.decode(this)
--- scaffold/src/desktopMain/kotlin/com/tunjid/heron/scaffold/navigation/StringEncoding.jvm.kt
+++
@@ -1,9 +0,0 @@
-package com.tunjid.heron.scaffold.navigation
-
-import java.net.URLDecoder
-import java.net.URLEncoder
-
-
-internal actual fun String.encodeUrl(): String = URLEncoder.encode(this)
-
-internal actual fun String.decodeUrl(): String = URLDecoder.decode(this)
