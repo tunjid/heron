4484a6905eafb3ac5a0a72f460626f19a207da38
Adetunji Dahunsi <tjdah100@gmail.com>
2025-01-06 23:01:00 +0100

    Pull Post UI component out to be used from other modules

--- ui-timeline/src/commonMain/kotlin/com/tunjid/heron/timeline/ui/TimelineItem.kt
+++ ui-timeline/src/commonMain/kotlin/com/tunjid/heron/timeline/ui/TimelineItem.kt
@@ -1,12 +1,9 @@
 package com.tunjid.heron.timeline.ui
 
-import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -26,33 +22,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import com.tunjid.heron.data.core.models.Embed
 import com.tunjid.heron.data.core.models.Post
 import com.tunjid.heron.data.core.models.Profile
 import com.tunjid.heron.data.core.models.TimelineItem
 import com.tunjid.heron.data.core.types.Uri
-import com.tunjid.heron.images.AsyncImage
-import com.tunjid.heron.images.ImageArgs
+import com.tunjid.heron.timeline.ui.post.Post
+import com.tunjid.heron.timeline.ui.post.PostReasonLine
+import com.tunjid.heron.timeline.utilities.createdAt
+import com.tunjid.heron.ui.SharedElementScope
 import com.tunjid.heron.ui.shapes.RoundedPolygonShape
 import com.tunjid.heron.ui.shapes.toRoundedPolygonShape
-import com.tunjid.heron.timeline.utilities.createdAt
-import com.tunjid.heron.timeline.utilities.format
-import com.tunjid.heron.ui.AttributionLayout
-import com.tunjid.heron.ui.SharedElementScope
-import com.tunjid.heron.timeline.ui.post.PostActions
-import com.tunjid.heron.timeline.ui.post.PostDate
-import com.tunjid.heron.timeline.ui.post.PostEmbed
-import com.tunjid.heron.timeline.ui.post.PostHeadline
-import com.tunjid.heron.timeline.ui.post.PostReasonLine
-import com.tunjid.heron.timeline.ui.post.PostText
-import com.tunjid.treenav.compose.moveablesharedelement.updatedMovableSharedElementOf
 import heron.ui_timeline.generated.resources.Res
 import heron.ui_timeline.generated.resources.see_more_posts
 import kotlinx.datetime.Instant
@@ -105,22 +89,23 @@ fun TimelineItem(
                 onPostClicked = onPostClicked,
                 onImageClicked = onImageClicked,
                 onReplyToPost = onReplyToPost
-            ) else SinglePost(
-                sharedElementScope = sharedElementScope,
-                now = now,
-                post = item.post,
-                embed = item.post.embed,
-                isAnchoredInTimeline = false,
-                avatarShape =
-                if (item is TimelineItem.Thread) ReplyThreadEndImageShape
-                else RoundedPolygonShape.Circle,
-                sharedElementPrefix = sharedElementPrefix,
-                createdAt = item.post.createdAt,
-                onProfileClicked = onProfileClicked,
-                onPostClicked = onPostClicked,
-                onImageClicked = onImageClicked,
-                onReplyToPost = onReplyToPost,
-            )
+            ) else
+                Post(
+                    sharedElementScope = sharedElementScope,
+                    now = now,
+                    post = item.post,
+                    embed = item.post.embed,
+                    isAnchoredInTimeline = false,
+                    avatarShape =
+                    if (item is TimelineItem.Thread) ReplyThreadEndImageShape
+                    else RoundedPolygonShape.Circle,
+                    sharedElementPrefix = sharedElementPrefix,
+                    createdAt = item.post.createdAt,
+                    onProfileClicked = onProfileClicked,
+                    onPostClicked = onPostClicked,
+                    onImageClicked = onImageClicked,
+                    onReplyToPost = onReplyToPost,
+                )
         }
     }
 }
@@ -139,7 +124,8 @@ private fun ThreadedPost(
     Column {
         item.posts.forEachIndexed { index, post ->
             if (index == 0 || item.posts[index].cid != item.posts[index - 1].cid) {
-                SinglePost(
+
+                Post(
                     sharedElementScope = sharedElementScope,
                     now = now,
                     post = post,
@@ -191,134 +177,6 @@ private fun ThreadedPost(
             }
         }
     }
-}
-
-@Composable
-private fun SinglePost(
-    sharedElementScope: SharedElementScope,
-    now: Instant,
-    post: Post,
-    embed: Embed?,
-    isAnchoredInTimeline: Boolean,
-    avatarShape: RoundedPolygonShape,
-    sharedElementPrefix: String,
-    createdAt: Instant,
-    onProfileClicked: (Post, Profile) -> Unit,
-    onPostClicked: (Post) -> Unit,
-    onImageClicked: (Uri) -> Unit,
-    onReplyToPost: () -> Unit,
-    timeline: @Composable (BoxScope.() -> Unit) = {},
-) {
-    Box {
-        timeline()
-        Column(
-            modifier = Modifier,
-        ) {
-            PostAttribution(
-                sharedElementScope = sharedElementScope,
-                avatarShape = avatarShape,
-                onProfileClicked = onProfileClicked,
-                post = post,
-                sharedElementPrefix = sharedElementPrefix,
-                now = now,
-                createdAt = createdAt,
-            )
-            Spacer(Modifier.height(4.dp))
-            Column(
-                modifier = Modifier.padding(
-                    start = 24.dp,
-                    bottom = 8.dp
-                ),
-                verticalArrangement = spacedBy(8.dp),
-            ) {
-                PostText(
-                    post = post,
-                    sharedElementPrefix = sharedElementPrefix,
-                    sharedElementScope = sharedElementScope,
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    onClick = { onPostClicked(post) },
-                    onProfileClicked = onProfileClicked
-                )
-                PostEmbed(
-                    now = now,
-                    embed = embed,
-                    quote = post.quote,
-                    postId = post.cid,
-                    sharedElementPrefix = sharedElementPrefix,
-                    sharedElementScope = sharedElementScope,
-                    onOpenImage = onImageClicked,
-                    onPostClicked = onPostClicked,
-                )
-                if (isAnchoredInTimeline) PostDate(
-                    modifier = Modifier.padding(
-                        vertical = 8.dp,
-                    ),
-                    time = post.createdAt,
-                )
-                PostActions(
-                    replyCount = format(post.replyCount),
-                    repostCount = format(post.repostCount),
-                    likeCount = format(post.likeCount),
-                    reposted = post.viewerStats?.reposted == true,
-                    liked = post.viewerStats?.liked == true,
-                    iconSize = 16.dp,
-                    postId = post.cid,
-                    sharedElementPrefix = sharedElementPrefix,
-                    sharedElementScope = sharedElementScope,
-                    onReplyToPost = onReplyToPost,
-                )
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalSharedTransitionApi::class)
-@Composable
-private fun PostAttribution(
-    sharedElementScope: SharedElementScope,
-    avatarShape: RoundedPolygonShape,
-    onProfileClicked: (Post, Profile) -> Unit,
-    post: Post,
-    sharedElementPrefix: String,
-    now: Instant,
-    createdAt: Instant,
-) = with(sharedElementScope) {
-    AttributionLayout(
-        avatar = {
-            updatedMovableSharedElementOf(
-                modifier = Modifier
-                    .size(48.dp)
-                    .clip(avatarShape)
-                    .clickable { onProfileClicked(post, post.author) },
-                key = post.avatarSharedElementKey(sharedElementPrefix),
-                state = remember(post.author.avatar) {
-                    ImageArgs(
-                        url = post.author.avatar?.uri,
-                        contentScale = ContentScale.Crop,
-                        contentDescription = post.author.displayName ?: post.author.handle.id,
-                        shape = avatarShape,
-                    )
-                },
-                sharedElement = { state, modifier ->
-                    AsyncImage(state, modifier)
-                }
-            )
-        },
-        label = {
-            PostHeadline(
-                now = now,
-                createdAt = createdAt,
-                author = post.author,
-                postId = post.cid,
-                sharedElementPrefix = sharedElementPrefix,
-                sharedElementScope = sharedElementScope,
-            )
-        }
-    )
-    //                if (item is TimelineItem.Reply) {
-//                    PostReplyLine(item.parentPost.author, onProfileClicked)
-//                }
 }
 
 @Composable
---
+++ ui-timeline/src/commonMain/kotlin/com/tunjid/heron/timeline/ui/post/Post.kt
@@ -0,0 +1,162 @@
+package com.tunjid.heron.timeline.ui.post
+
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement.spacedBy
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.tunjid.heron.data.core.models.Embed
+import com.tunjid.heron.data.core.models.Post
+import com.tunjid.heron.data.core.models.Profile
+import com.tunjid.heron.data.core.types.Uri
+import com.tunjid.heron.images.AsyncImage
+import com.tunjid.heron.images.ImageArgs
+import com.tunjid.heron.timeline.ui.avatarSharedElementKey
+import com.tunjid.heron.timeline.utilities.createdAt
+import com.tunjid.heron.timeline.utilities.format
+import com.tunjid.heron.ui.AttributionLayout
+import com.tunjid.heron.ui.SharedElementScope
+import com.tunjid.heron.ui.shapes.RoundedPolygonShape
+import com.tunjid.treenav.compose.moveablesharedelement.updatedMovableSharedElementOf
+import kotlinx.datetime.Instant
+
+@Composable
+fun Post(
+    sharedElementScope: SharedElementScope,
+    now: Instant,
+    post: Post,
+    embed: Embed?,
+    isAnchoredInTimeline: Boolean,
+    avatarShape: RoundedPolygonShape,
+    sharedElementPrefix: String,
+    createdAt: Instant,
+    onProfileClicked: (Post, Profile) -> Unit,
+    onPostClicked: (Post) -> Unit,
+    onImageClicked: (Uri) -> Unit,
+    onReplyToPost: () -> Unit,
+    timeline: @Composable (BoxScope.() -> Unit) = {},
+) {
+    Box {
+        timeline()
+        Column(
+            modifier = Modifier,
+        ) {
+            PostAttribution(
+                sharedElementScope = sharedElementScope,
+                avatarShape = avatarShape,
+                onProfileClicked = onProfileClicked,
+                post = post,
+                sharedElementPrefix = sharedElementPrefix,
+                now = now,
+                createdAt = createdAt,
+            )
+            Spacer(Modifier.height(4.dp))
+            Column(
+                modifier = Modifier.padding(
+                    start = 24.dp,
+                    bottom = 8.dp
+                ),
+                verticalArrangement = spacedBy(8.dp),
+            ) {
+                PostText(
+                    post = post,
+                    sharedElementPrefix = sharedElementPrefix,
+                    sharedElementScope = sharedElementScope,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    onClick = { onPostClicked(post) },
+                    onProfileClicked = onProfileClicked
+                )
+                PostEmbed(
+                    now = now,
+                    embed = embed,
+                    quote = post.quote,
+                    postId = post.cid,
+                    sharedElementPrefix = sharedElementPrefix,
+                    sharedElementScope = sharedElementScope,
+                    onOpenImage = onImageClicked,
+                    onPostClicked = onPostClicked,
+                )
+                if (isAnchoredInTimeline) PostDate(
+                    modifier = Modifier.padding(
+                        vertical = 8.dp,
+                    ),
+                    time = post.createdAt,
+                )
+                PostActions(
+                    replyCount = format(post.replyCount),
+                    repostCount = format(post.repostCount),
+                    likeCount = format(post.likeCount),
+                    reposted = post.viewerStats?.reposted == true,
+                    liked = post.viewerStats?.liked == true,
+                    iconSize = 16.dp,
+                    postId = post.cid,
+                    sharedElementPrefix = sharedElementPrefix,
+                    sharedElementScope = sharedElementScope,
+                    onReplyToPost = onReplyToPost,
+                )
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+private fun PostAttribution(
+    sharedElementScope: SharedElementScope,
+    avatarShape: RoundedPolygonShape,
+    onProfileClicked: (Post, Profile) -> Unit,
+    post: Post,
+    sharedElementPrefix: String,
+    now: Instant,
+    createdAt: Instant,
+) = with(sharedElementScope) {
+    AttributionLayout(
+        avatar = {
+            updatedMovableSharedElementOf(
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(avatarShape)
+                    .clickable { onProfileClicked(post, post.author) },
+                key = post.avatarSharedElementKey(sharedElementPrefix),
+                state = remember(post.author.avatar) {
+                    ImageArgs(
+                        url = post.author.avatar?.uri,
+                        contentScale = ContentScale.Crop,
+                        contentDescription = post.author.displayName ?: post.author.handle.id,
+                        shape = avatarShape,
+                    )
+                },
+                sharedElement = { state, modifier ->
+                    AsyncImage(state, modifier)
+                }
+            )
+        },
+        label = {
+            PostHeadline(
+                now = now,
+                createdAt = createdAt,
+                author = post.author,
+                postId = post.cid,
+                sharedElementPrefix = sharedElementPrefix,
+                sharedElementScope = sharedElementScope,
+            )
+        }
+    )
+    //                if (item is TimelineItem.Reply) {
+//                    PostReplyLine(item.parentPost.author, onProfileClicked)
+//                }
+}
