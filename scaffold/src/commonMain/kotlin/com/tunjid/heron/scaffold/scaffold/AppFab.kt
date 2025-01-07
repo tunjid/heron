cc15c88ea1e5ba0b82c3bf57ff4d13a198527e13
Adetunji Dahunsi <tjdah100@gmail.com>
2025-01-07 16:04:53 +0100

    Use a custom expanding fab using a regular fab

--- scaffold/src/commonMain/kotlin/com/tunjid/heron/scaffold/scaffold/AppFab.kt
+++ scaffold/src/commonMain/kotlin/com/tunjid/heron/scaffold/scaffold/AppFab.kt
@@ -1,12 +1,22 @@
 package com.tunjid.heron.scaffold.scaffold
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Edit
-import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,7 +26,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalDensity
@@ -36,29 +48,61 @@ fun Fab(
     expanded: Boolean,
     onClick: () -> Unit,
 ) = with(sharedElementScope) {
-    ExtendedFloatingActionButton(
+    // The material3 ExtendedFloatingActionButton does not allow for placing
+    // Modifier.animateContentSize() on its row.
+    FloatingActionButton(
         modifier = modifier
+            .wrapContentHeight()
             .animateContentSize()
             .sharedElement(
                 key = FabSharedElementKey,
                 zIndexInOverlay = FabSharedElementZIndex,
             ),
         onClick = onClick,
-        expanded = expanded,
-        shape = RoundedCornerShape(64.dp),
-        text = {
-            Text(
-                text = text,
-                maxLines = 1,
-            )
-        },
-        icon = {
-            Icon(
-                imageVector = icon,
-                contentDescription = text
-            )
+        shape = MaterialTheme.shapes.small.copy(CornerSize(percent = 50)),
+        content = {
+            Row(
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                FabIcon(icon)
+                if (expanded) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                    AnimatedContent(targetState = text) { text ->
+                        Text(
+                            text = text,
+                            maxLines = 1,
+                        )
+                    }
+                }
+            }
         }
     )
+}
+
+@Composable
+private fun FabIcon(icon: ImageVector) {
+    val rotationAnimatable = remember { Animatable(initialValue = 0f) }
+    val animationSpec = remember {
+        spring(
+            dampingRatio = Spring.DampingRatioLowBouncy,
+            stiffness = Spring.StiffnessHigh,
+            visibilityThreshold = 0.1f
+        )
+    }
+
+    Icon(
+        modifier = Modifier.rotate(rotationAnimatable.value),
+        imageVector = icon,
+        contentDescription = null
+    )
+
+    LaunchedEffect(icon) {
+        rotationAnimatable.animateTo(targetValue = 30f, animationSpec = animationSpec)
+        rotationAnimatable.animateTo(targetValue = 0f, animationSpec = animationSpec)
+    }
 }
 
 @Composable
