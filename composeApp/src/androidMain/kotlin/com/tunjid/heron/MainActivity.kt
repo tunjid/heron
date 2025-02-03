a0dc5645c2ac4d81bbd87ec10cd876e700be94d7
Adetunji Dahunsi <tjdah100@gmail.com>
2025-02-03 17:57:20 -0500

    Add application subclass for android

--- composeApp/src/androidMain/kotlin/com/tunjid/heron/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/tunjid/heron/MainActivity.kt
@@ -16,17 +16,14 @@
 
 package com.tunjid.heron
 
-import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.AndroidViewModel
 import com.tunjid.heron.scaffold.navigation.PredictiveBackEffects
 import com.tunjid.heron.scaffold.scaffold.App
 import io.github.vinceglb.filekit.core.FileKit
@@ -37,8 +34,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         FileKit.init(this)
 
-        val appViewModel by viewModels<AppViewModel>()
-        val appState = appViewModel.appState
+        val appState = (application as HeronApplication).appState
 
         setContent {
             App(
@@ -50,9 +46,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-class AppViewModel(app: Application) : AndroidViewModel(app) {
-    val appState = createAppState(app)
-}
 
 @Preview
 @Composable
