ed0ffd37e197e0588c4a963df6cf0b63419259e5
Adetunji Dahunsi <tjdah100@gmail.com>
2025-02-02 20:15:21 -0500

    Poll for notifications

--- data/src/commonMain/kotlin/com/tunjid/heron/data/repository/NotificationsRepository.kt
+++ data/src/commonMain/kotlin/com/tunjid/heron/data/repository/NotificationsRepository.kt
@@ -41,6 +41,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.datetime.Clock
@@ -76,15 +77,21 @@ class OfflineNotificationsRepository @Inject constructor(
     override val unreadCount: Flow<Long> = savedStateRepository.savedState
         .map { it.notifications?.lastSeen }
         .distinctUntilChanged()
-        .map { lastSeen ->
-            runCatchingWithNetworkRetry {
-                networkService.api.getUnreadCount(
-                    params = GetUnreadCountQueryParams(
-                        seenAt = lastSeen
-                    )
-                )
+        .flatMapLatest { lastSeen ->
+            flow {
+                while (true) {
+                    val unreadCount = runCatchingWithNetworkRetry {
+                        networkService.api.getUnreadCount(
+                            params = GetUnreadCountQueryParams(
+                                seenAt = lastSeen
+                            )
+                        )
+                    }
+                        .getOrNull()?.count ?: 0
+                    emit(unreadCount)
+                    kotlinx.coroutines.delay(10_000)
+                }
             }
-                .getOrNull()?.count ?: 0
         }
         .stateIn(
             scope = appScope,
