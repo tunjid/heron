2c9d7410e5527c2dbc131fc0f08cec62649dc311
Adetunji Dahunsi <tjdah100@gmail.com>
2025-07-20 14:14:58 -0700

    Add definitions for messaging entities

---
+++ data-database/src/commonMain/kotlin/com/tunjid/heron/data/database/entities/messageembeds/MessageListEntity.kt
@@ -0,0 +1,58 @@
+/*
+ *    Copyright 2024 Adetunji Dahunsi
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.tunjid.heron.data.database.entities.messageembeds
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import com.tunjid.heron.data.core.types.ListId
+import com.tunjid.heron.data.core.types.MessageId
+import com.tunjid.heron.data.database.entities.ListEntity
+import com.tunjid.heron.data.database.entities.MessageEntity
+
+
+@Entity(
+    tableName = "messageLists",
+    primaryKeys = [
+        "messageId",
+        "listId"
+    ],
+    foreignKeys = [
+        ForeignKey(
+            entity = MessageEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["messageId"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+        ForeignKey(
+            entity = ListEntity::class,
+            parentColumns = ["cid"],
+            childColumns = ["listId"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ],
+    indices = [
+        Index(value = ["messageId"]),
+        Index(value = ["listId"]),
+    ],
+)
+data class MessageListEntity(
+    val messageId: MessageId,
+    val listId: ListId,
+)
+
+
---
+++ data-database/src/commonMain/kotlin/com/tunjid/heron/data/database/entities/messageembeds/MessagePostEntity.kt
@@ -0,0 +1,58 @@
+/*
+ *    Copyright 2024 Adetunji Dahunsi
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.tunjid.heron.data.database.entities.messageembeds
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import com.tunjid.heron.data.core.types.MessageId
+import com.tunjid.heron.data.core.types.PostId
+import com.tunjid.heron.data.database.entities.MessageEntity
+import com.tunjid.heron.data.database.entities.PostEntity
+
+
+@Entity(
+    tableName = "messagePosts",
+    primaryKeys = [
+        "messageId",
+        "postId"
+    ],
+    foreignKeys = [
+        ForeignKey(
+            entity = MessageEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["messageId"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+        ForeignKey(
+            entity = PostEntity::class,
+            parentColumns = ["cid"],
+            childColumns = ["postId"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ],
+    indices = [
+        Index(value = ["messageId"]),
+        Index(value = ["postId"]),
+    ],
+)
+data class MessagePostEntity(
+    val messageId: MessageId,
+    val postId: PostId,
+)
+
+
