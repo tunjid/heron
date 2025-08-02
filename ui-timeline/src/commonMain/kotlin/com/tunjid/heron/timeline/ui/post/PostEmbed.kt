310a67ce624d2bd4695d5122619881f8787cac39
Adetunji Dahunsi <tjdah100@gmail.com>
2025-08-02 11:12:51 -0700

    Simplify blurring code

--- ui-timeline/src/commonMain/kotlin/com/tunjid/heron/timeline/ui/post/PostEmbed.kt
+++ ui-timeline/src/commonMain/kotlin/com/tunjid/heron/timeline/ui/post/PostEmbed.kt
@@ -16,6 +16,7 @@
 
 package com.tunjid.heron.timeline.ui.post
 
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -36,6 +37,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import com.tunjid.heron.data.core.models.Constants
@@ -78,14 +80,11 @@ internal fun PostEmbed(
     presentation: Timeline.Presentation,
 ) {
     val uriHandler = LocalUriHandler.current
-    var hasClickedThroughBlurredMedia by rememberSaveable(postId) {
-        mutableStateOf(false)
-    }
-    val isBlurred = blurredMediaDefinitions.isNotEmpty() && !hasClickedThroughBlurredMedia
-
-    Box(
-        modifier = modifier
-    ) {
+    SensitiveContentBox(
+        modifier = modifier,
+        postId = postId,
+        blurredMediaDefinitions = blurredMediaDefinitions
+    ) { isBlurred ->
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -154,26 +153,50 @@ internal fun PostEmbed(
                 }
             }
         }
+    }
+}
 
-        val canClickThrough = blurredMediaDefinitions.none {
-            it.severity == Label.Severity.None
-        }
-        if (isBlurred && canClickThrough && !hasClickedThroughBlurredMedia) {
+@Composable
+private fun SensitiveContentBox(
+    modifier: Modifier,
+    postId: PostId,
+    blurredMediaDefinitions: List<Label.Definition>,
+    content: @Composable (isBlurred: Boolean) -> Unit,
+) {
+    var hasClickedThroughBlurredMedia by rememberSaveable(postId) {
+        mutableStateOf(false)
+    }
+    val isBlurred = blurredMediaDefinitions.isNotEmpty() && !hasClickedThroughBlurredMedia
+
+    Box(
+        modifier = modifier,
+        content = {
             Box(
-                modifier = Modifier
-                    .matchParentSize()
-//                    .background(Color.Black.copy(alpha = 0.6f))
-            )
-            SensitiveContentButton(
-                modifier = Modifier
-                    .align(Alignment.Center),
-                blurredMediaDefinitions = blurredMediaDefinitions,
-                onClick = {
-                    hasClickedThroughBlurredMedia = true
+                modifier = when {
+                    // Prevent clicks on other things
+                    isBlurred -> Modifier.pointerInput(Unit) { detectTapGestures() }
+                    else -> Modifier
+                },
+                content = {
+                    content(isBlurred)
                 },
             )
+
+            val canClickThrough = blurredMediaDefinitions.none {
+                it.severity == Label.Severity.None
+            }
+            if (isBlurred && canClickThrough && !hasClickedThroughBlurredMedia) {
+                SensitiveContentButton(
+                    modifier = Modifier
+                        .align(Alignment.Center),
+                    blurredMediaDefinitions = blurredMediaDefinitions,
+                    onClick = {
+                        hasClickedThroughBlurredMedia = true
+                    },
+                )
+            }
         }
-    }
+    )
 }
 
 @Composable
