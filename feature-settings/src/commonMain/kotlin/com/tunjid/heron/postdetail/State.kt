33cec71c3c59b6019857e07c29921512c2fc82a8
joelmuraguri <joelkmuraguri21@gmail.com>
2025-08-20 22:00:26 +0300

    add feature-settings-module

---
+++ feature-settings/src/commonMain/kotlin/com/tunjid/heron/postdetail/State.kt
@@ -0,0 +1,42 @@
+/*
+ *    Copyright 2024 Adetunji Dahunsi
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.tunjid.heron.postdetail
+
+import com.tunjid.heron.scaffold.navigation.NavigationAction
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+
+
+@Serializable
+data class State(
+    @Transient
+    val messages: List<String> = emptyList(),
+)
+
+sealed class Action(val key: String) {
+
+    data object SignOut : Action(key = "SignOut")
+
+    sealed class Navigate : Action(key = "Navigate"), NavigationAction {
+        data object Pop : Navigate(), NavigationAction by NavigationAction.Pop
+
+        /** Handles navigation to settings child screens */
+        data class To(
+            val delegate: NavigationAction.Destination,
+        ) : Navigate(), NavigationAction by delegate
+    }
+}
