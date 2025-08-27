6089fdecedb019da73e40686c2500be06d89bce8
joelmuraguri <joelkmuraguri21@gmail.com>
2025-08-27 09:31:42 +0300

    create a transaction block for upsertInteraction and updateLikeCount to run simultaneously

--- data/core/src/commonMain/kotlin/com/tunjid/heron/data/repository/PostRepository.kt
+++ data/core/src/commonMain/kotlin/com/tunjid/heron/data/repository/PostRepository.kt
@@ -389,24 +389,29 @@ internal class OfflinePostRepository @Inject constructor(
                 .getOrNull()
                 ?.let {
                     if (it.validationStatus !is CreateRecordValidationStatus.Valid) return@let
-                    upsertInteraction(
-                        partial = when (interaction) {
-                            is Post.Interaction.Create.Like -> {
-                                PostViewerStatisticsEntity.Partial.Like(
-                                    likeUri = it.uri.atUri.let(::GenericUri),
+                    transactionWriter.inTransaction {
+                        upsertInteraction(
+                            partial = when (interaction) {
+                                is Post.Interaction.Create.Like -> {
+                                    PostViewerStatisticsEntity.Partial.Like(
+                                        likeUri = it.uri.atUri.let(::GenericUri),
+                                        postUri = interaction.postUri,
+                                        viewingProfileId = authorId,
+                                    )
+                                }
+
+                                is Post.Interaction.Create.Repost -> PostViewerStatisticsEntity.Partial.Repost(
+                                    repostUri = it.uri.atUri.let(::GenericUri),
                                     postUri = interaction.postUri,
                                     viewingProfileId = authorId,
                                 )
                             }
-
-                            is Post.Interaction.Create.Repost -> PostViewerStatisticsEntity.Partial.Repost(
-                                repostUri = it.uri.atUri.let(::GenericUri),
-                                postUri = interaction.postUri,
-                                viewingProfileId = authorId,
-                            )
-                        }
-                    )
-                    postDao.updateLikeCount(postUri =interaction.postUri.uri, isIncrement = true)
+                        )
+                        postDao.updateLikeCount(
+                            postUri =interaction.postUri.uri,
+                            isIncrement = true
+                        )
+                    }
                 }
 
             is Post.Interaction.Delete -> networkService.runCatchingWithMonitoredNetworkRetry {
@@ -430,24 +435,29 @@ internal class OfflinePostRepository @Inject constructor(
             }
                 .getOrNull()
                 ?.let {
-                    upsertInteraction(
-                        partial = when (interaction) {
-                            is Post.Interaction.Delete.Unlike -> {
-                                PostViewerStatisticsEntity.Partial.Like(
-                                    likeUri = null,
+                    transactionWriter.inTransaction {
+                        upsertInteraction(
+                            partial = when (interaction) {
+                                is Post.Interaction.Delete.Unlike -> {
+                                    PostViewerStatisticsEntity.Partial.Like(
+                                        likeUri = null,
+                                        postUri = interaction.postUri,
+                                        viewingProfileId = authorId,
+                                    )
+                                }
+
+                                is Post.Interaction.Delete.RemoveRepost -> PostViewerStatisticsEntity.Partial.Repost(
+                                    repostUri = null,
                                     postUri = interaction.postUri,
                                     viewingProfileId = authorId,
                                 )
                             }
-
-                            is Post.Interaction.Delete.RemoveRepost -> PostViewerStatisticsEntity.Partial.Repost(
-                                repostUri = null,
-                                postUri = interaction.postUri,
-                                viewingProfileId = authorId,
-                            )
-                        }
-                    )
-                    postDao.updateLikeCount(postUri =interaction.postUri.uri, isIncrement = false)
+                        )
+                        postDao.updateLikeCount(
+                            postUri =interaction.postUri.uri,
+                            isIncrement = false
+                        )
+                    }
                 }
         }
 
