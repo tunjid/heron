1b3723f67d3326ed6ad2407f2e0115a0e0059d32
joelmuraguri <joelkmuraguri21@gmail.com>
2025-09-17 23:47:24 +0300

    Configure ProtoBuf to also ignore unknown keys

--- data/models/src/desktopTest/kotlin/com/tunjid/heron/helpers/SerializationTestHelper.kt
+++ data/models/src/desktopTest/kotlin/com/tunjid/heron/helpers/SerializationTestHelper.kt
@@ -9,7 +9,7 @@ import kotlinx.serialization.protobuf.ProtoBuf
 object SerializationTestHelper {
 
     private val cbor = Cbor { ignoreUnknownKeys = true }
-    private val proto = ProtoBuf
+    private val proto = ProtoBuf { ignoreUnknownKeys = true }
 
     enum class Format {
         CBOR,
