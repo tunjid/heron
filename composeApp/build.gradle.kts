753f21859f0f4919807e6c68eaa6def8d986750d
Adetunji Dahunsi <tjdah100@gmail.com>
2025-09-28 13:09:55 -0700

    Version bump

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -109,14 +109,14 @@ kotlin {
     }
 }
 
-val appVersion = "1.0.5"
+val appVersion = "1.0.6"
 
 android {
     namespace = "com.tunjid.heron"
 
     defaultConfig {
         applicationId = "com.tunjid.heron"
-        versionCode = 6
+        versionCode = 7
         versionName = appVersion
     }
     packaging {
