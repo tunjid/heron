56927ece3c458957321696c8a061372c9978428a
joelmuraguri <joelkmuraguri21@gmail.com>
2025-09-30 00:17:59 +0300

    add burst library

--- data/core/build.gradle.kts
+++ data/core/build.gradle.kts
@@ -83,6 +83,7 @@ kotlin {
         commonTest {
             dependencies {
                 implementation(kotlin("test"))
+                implementation(libs.burst)
             }
         }
     }
