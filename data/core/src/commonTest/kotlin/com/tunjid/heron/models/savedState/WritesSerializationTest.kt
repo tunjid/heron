1dc53e2f85b13fb29bc8417d23ed7c5244ebabf4
joelmuraguri <joelkmuraguri21@gmail.com>
2025-09-30 09:06:43 +0300

    Writes model serialization test

---
+++ data/core/src/commonTest/kotlin/com/tunjid/heron/models/savedState/WritesSerializationTest.kt
@@ -0,0 +1,33 @@
+package com.tunjid.heron.models.savedState
+
+import app.cash.burst.Burst
+import app.cash.burst.burstValues
+import com.tunjid.heron.data.repository.SavedState
+import com.tunjid.heron.fakes.SampleSavedStateData
+import com.tunjid.heron.helper.SerializationTestHelper
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlinx.serialization.ExperimentalSerializationApi
+
+@OptIn(ExperimentalSerializationApi::class)
+@Burst
+class WritesSerializationTest(
+    val format: SerializationTestHelper.Format = burstValues(
+        SerializationTestHelper.Format.CBOR,
+        SerializationTestHelper.Format.PROTOBUF,
+    ),
+    val original: SavedState.Writes = burstValues(
+        SavedState.Writes(),
+        SampleSavedStateData.writes(),
+    ),
+) {
+    @Test
+    fun roundTrip() {
+        val decoded = SerializationTestHelper.roundTrip(
+            format = format,
+            value = original,
+            serializer = SavedState.Writes.serializer()
+        )
+        assertEquals(original, decoded)
+    }
+}
