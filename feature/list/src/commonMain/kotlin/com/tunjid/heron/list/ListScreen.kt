ba040055e45e193afa489da8856139f96ba0adf5
joelmuraguri <joelkmuraguri21@gmail.com>
2025-10-23 21:02:50 +0300

    handle onPostOptionsClicked in List Screen

--- feature/list/src/commonMain/kotlin/com/tunjid/heron/list/ListScreen.kt
+++ feature/list/src/commonMain/kotlin/com/tunjid/heron/list/ListScreen.kt
@@ -86,6 +86,7 @@ import com.tunjid.heron.timeline.ui.TimelineItem
 import com.tunjid.heron.timeline.ui.avatarSharedElementKey
 import com.tunjid.heron.timeline.ui.effects.TimelineRefreshEffect
 import com.tunjid.heron.timeline.ui.post.PostInteractionsSheetState.Companion.rememberUpdatedPostInteractionState
+import com.tunjid.heron.timeline.ui.post.PostOptionsSheetState.Companion.rememberUpdatedPostOptionsState
 import com.tunjid.heron.timeline.ui.post.threadtraversal.ThreadedVideoPositionState.Companion.threadedVideoPosition
 import com.tunjid.heron.timeline.ui.post.threadtraversal.ThreadedVideoPositionStates
 import com.tunjid.heron.timeline.ui.postActions
@@ -375,21 +376,26 @@ private fun ListTimeline(
                 ),
             )
         },
+    )
+
+    val postOptionsState = rememberUpdatedPostOptionsState(
+        isSignedIn = paneScaffoldState.isSignedIn,
         recentConversations = recentConversations,
-        onShareInConversationClicked = { conversation, postUri ->
+        onShareInConversationClicked = { currentPost, conversation ->
             actions(
                 Action.Navigate.To(
                     conversationDestination(
                         id = conversation.id,
                         members = conversation.members,
                         sharedElementPrefix = conversation.id.id,
-                        sharedPostUri = postUri,
+                        sharedPostUri = currentPost.uri,
                         referringRouteOption = NavigationAction.ReferringRouteOption.Current,
                     ),
                 ),
             )
         },
     )
+
     LookaheadScope {
         LazyVerticalStaggeredGrid(
             modifier = Modifier
@@ -513,6 +519,7 @@ private fun ListTimeline(
                                     )
                                 },
                                 onPostInteraction = postInteractionState::onInteraction,
+                                onPostOptionsClicked = postOptionsState::showOptions,
                             )
                         },
                     )
