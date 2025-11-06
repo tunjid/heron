1dae90620f07a88659b16d6a3ea1b7de951293c2
joelmuraguri <joelkmuraguri21@gmail.com>
2025-11-06 15:18:25 +0300

    add label entity

---
+++ data/database/src/commonMain/kotlin/com/tunjid/heron/data/database/entities/LabelerEntity.kt
@@ -0,0 +1,49 @@
+package com.tunjid.heron.data.database.entities
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.Relation
+import com.tunjid.heron.data.core.types.GenericUri
+import com.tunjid.heron.data.core.types.ProfileId
+
+@Entity(
+    tableName = "labelers",
+    primaryKeys = [
+        "uri",
+    ],
+    foreignKeys = [
+        ForeignKey(
+            entity = ProfileEntity::class,
+            parentColumns = ["did"],
+            childColumns = ["creatorId"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ],
+    indices = [
+        Index(value = ["uri"]),
+        Index(value = ["creatorId"]),
+    ],
+)
+data class LabelerEntity(
+    val cid: String,
+    val uri: GenericUri,
+    val creatorId: ProfileId,
+    val likeCount: Long?,
+)
+
+data class PopulatedLabelerEntity(
+    @Embedded
+    val entity: LabelerEntity,
+    @Relation(
+        parentColumn = "creatorId",
+        entityColumn = "did",
+    )
+    val creator: ProfileEntity?,
+    @Relation(
+        parentColumn = "creatorId",
+        entityColumn = "creatorId",
+    )
+    val definitions: List<LabelDefinitionEntity>,
+)
