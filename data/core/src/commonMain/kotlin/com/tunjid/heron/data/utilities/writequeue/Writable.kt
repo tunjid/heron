b410771d56c24ee9611c9786500834456a8e0d98
Adetunji Dahunsi <tjdah100@gmail.com>
2025-11-09 17:08:01 -0500

    ktlint

--- data/core/src/commonMain/kotlin/com/tunjid/heron/data/utilities/writequeue/Writable.kt
+++ data/core/src/commonMain/kotlin/com/tunjid/heron/data/utilities/writequeue/Writable.kt
@@ -126,7 +126,7 @@ sealed interface Writable {
                 is Timeline.Update.OfFeedGenerator.Pin -> "pin-${update.uri}"
                 is Timeline.Update.OfFeedGenerator.Remove -> "remove-${update.uri}"
                 is Timeline.Update.OfFeedGenerator.Save -> "save-${update.uri}"
-                is Timeline.Update.OfContentLabel.VisibilityChange -> "visibility-change-${update}"
+                is Timeline.Update.OfContentLabel.VisibilityChange -> "visibility-change-$update"
             }
 
         override suspend fun WriteQueue.write(): Outcome =
