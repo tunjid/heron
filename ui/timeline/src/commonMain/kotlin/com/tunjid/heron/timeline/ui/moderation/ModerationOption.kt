7ae1bb4bcd02bdd48b1ed94f07435d52274b4ec7
joelmuraguri <joelkmuraguri21@gmail.com>
2025-12-23 19:02:16 +0300

    use string resource

--- ui/timeline/src/commonMain/kotlin/com/tunjid/heron/timeline/ui/moderation/ModerationOption.kt
+++ ui/timeline/src/commonMain/kotlin/com/tunjid/heron/timeline/ui/moderation/ModerationOption.kt
@@ -3,27 +3,25 @@ package com.tunjid.heron.timeline.ui.moderation
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FilterAlt
 import androidx.compose.material.icons.rounded.Block
-import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.tunjid.heron.timeline.ui.sheets.MutedWordsStateHolder
+import heron.ui.timeline.generated.resources.Res
+import heron.ui.timeline.generated.resources.block_user
+import heron.ui.timeline.generated.resources.mute_words
+import org.jetbrains.compose.resources.StringResource
+
+const val ModerationFeatureWhileSubscribed = 2_000L
 
 sealed class ModerationOption(
-    val title: String,
+    val titleRes: StringResource,
     val icon: ImageVector,
 ) {
     data object MuteWords : ModerationOption(
-        title = "Mute words and tags",
+        titleRes = Res.string.mute_words,
         icon = Icons.Default.FilterAlt,
     )
 
     data object BlockUser : ModerationOption(
-        title = "Block user",
+        titleRes = Res.string.block_user,
         icon = Icons.Rounded.Block,
     )
 }
-
-@Stable
-data class ModerationState(
-    val mutedWordsStateHolder: MutedWordsStateHolder? = null,
-    // Add more moderation state holders as needed
-)
