2665ba184cbd2eebaa7ddfa07fd29a683eae55aa
joelmuraguri <joelkmuraguri21@gmail.com>
2026-01-15 01:14:03 +0300

    impl moderation section

--- ui/timeline/src/commonMain/kotlin/com/tunjid/heron/timeline/ui/post/PostOptionsSheetState.kt
+++ ui/timeline/src/commonMain/kotlin/com/tunjid/heron/timeline/ui/post/PostOptionsSheetState.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.VolumeOff
 import androidx.compose.material.icons.rounded.EditAttributes
+import androidx.compose.material.icons.rounded.FilterAlt
+import androidx.compose.material.icons.rounded.PersonOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.tunjid.heron.data.core.models.Conversation
 import com.tunjid.heron.data.core.models.Post
@@ -20,15 +24,20 @@ import com.tunjid.heron.data.core.types.ProfileId
 import com.tunjid.heron.timeline.utilities.BottomSheetItemCard
 import com.tunjid.heron.timeline.utilities.BottomSheetItemCardRow
 import com.tunjid.heron.timeline.utilities.CopyToClipboardCard
-import com.tunjid.heron.timeline.utilities.ModerationMenuSection
+import com.tunjid.heron.timeline.utilities.PostModerationMenuSection
 import com.tunjid.heron.timeline.utilities.SendDirectMessageCard
 import com.tunjid.heron.timeline.utilities.shareUri
 import com.tunjid.heron.ui.sheets.BottomSheetScope
 import com.tunjid.heron.ui.sheets.BottomSheetScope.Companion.ModalBottomSheet
 import com.tunjid.heron.ui.sheets.BottomSheetScope.Companion.rememberBottomSheetState
 import com.tunjid.heron.ui.sheets.BottomSheetState
+import com.tunjid.heron.ui.text.CommonStrings
+import heron.ui.core.generated.resources.viewer_state_block_account
+import heron.ui.core.generated.resources.viewer_state_mute_account
 import heron.ui.timeline.generated.resources.Res
+import heron.ui.timeline.generated.resources.mute_words_tags
 import heron.ui.timeline.generated.resources.thread_gate_post_reply_settings
+import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.stringResource
 
 @Stable
@@ -113,7 +122,8 @@ private fun PostOptionsBottomSheet(
                 },
             )
             state.currentPost?.let {
-                if (it.author.did == signedInProfileId) BottomSheetItemCard(
+                val isOwnPost = it.author.did == signedInProfileId
+                if (isOwnPost) BottomSheetItemCard(
                     modifier = Modifier
                         .fillMaxWidth(),
                     onClick = {
@@ -129,9 +139,12 @@ private fun PostOptionsBottomSheet(
                     },
                 )
                 CopyToClipboardCard(it.uri.shareUri())
-                ModerationMenuSection(
-                    onMuteWordsClicked = {
-                        onOptionClicked(PostOption.Moderation.MuteWords)
+                if (!isOwnPost) PostModerationMenuSection(
+                    signedInProfileId = signedInProfileId,
+                    post = it,
+                    onOptionClicked = { option ->
+                        state.hide()
+                        onOptionClicked(option)
                     },
                 )
             }
@@ -150,7 +163,35 @@ sealed class PostOption {
     ) : PostOption()
 
     sealed class Moderation : PostOption() {
+
         data object MuteWords : Moderation()
-        data object BlockUser : Moderation()
+
+        data class BlockAccount(
+            val signedInProfileId: ProfileId,
+            val post: Post,
+        ) : Moderation()
+
+        data class MuteAccount(
+            val signedInProfileId: ProfileId,
+            val post: Post,
+        ) : Moderation()
     }
+}
+
+enum class PostModerationTools(
+    val stringRes: StringResource,
+    val icon: ImageVector,
+) {
+    MuteWords(
+        stringRes = Res.string.mute_words_tags,
+        icon = Icons.Rounded.FilterAlt,
+    ),
+    BlockAccount(
+        stringRes = CommonStrings.viewer_state_block_account,
+        icon = Icons.Rounded.PersonOff,
+    ),
+    MuteAccount(
+        stringRes = CommonStrings.viewer_state_mute_account,
+        icon = Icons.AutoMirrored.Rounded.VolumeOff,
+    ),
 }
