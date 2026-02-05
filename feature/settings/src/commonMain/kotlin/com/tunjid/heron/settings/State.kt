76abecada67220444bf2a7851f786b82396f7fe3
joelmuraguri <joelkmuraguri21@gmail.com>
2026-02-05 23:59:30 +0300

    lock UI interaction and dim screen during account switching

--- feature/settings/src/commonMain/kotlin/com/tunjid/heron/settings/State.kt
+++ feature/settings/src/commonMain/kotlin/com/tunjid/heron/settings/State.kt
@@ -19,6 +19,7 @@ package com.tunjid.heron.settings
 import com.mikepenz.aboutlibraries.Libs
 import com.tunjid.heron.data.core.models.Preferences
 import com.tunjid.heron.data.core.models.SessionSummary
+import com.tunjid.heron.data.core.types.ProfileId
 import com.tunjid.heron.scaffold.navigation.NavigationAction
 import com.tunjid.heron.ui.text.Memo
 import kotlinx.serialization.Serializable
@@ -27,6 +28,7 @@ import kotlinx.serialization.Transient
 @Serializable
 data class State(
     val isSwitchingAccount: Boolean = false,
+    val switchingToProfile: ProfileId? = null,
     val signedInProfilePreferences: Preferences? = null,
     val openSourceLibraries: Libs? = null,
     val pastSessions: List<SessionSummary> = emptyList(),
