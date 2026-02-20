e9960772b5d806a2880b187abfccf332724372f9
Adetunji Dahunsi <tjdah100@gmail.com>
2026-02-20 08:13:42 -0500

    Move more errors

--- ui/core/src/commonMain/kotlin/com/tunjid/heron/ui/extensions/HeronException.kt
+++ ui/core/src/commonMain/kotlin/com/tunjid/heron/ui/extensions/HeronException.kt
@@ -16,8 +16,10 @@
 
 package com.tunjid.heron.ui.extensions
 
+import com.tunjid.heron.data.core.types.AtProtoException
 import com.tunjid.heron.data.core.types.ExpiredSessionException
 import com.tunjid.heron.data.core.types.HeronException
+import com.tunjid.heron.data.core.types.InvalidTokenException
 import com.tunjid.heron.data.core.types.MutedThreadException
 import com.tunjid.heron.data.core.types.NotificationFilteredOutException
 import com.tunjid.heron.data.core.types.RecordCreationException
@@ -28,9 +30,18 @@ import com.tunjid.heron.data.core.types.UnresolvableProfileException
 import com.tunjid.heron.data.core.types.UnresolvableRecordException
 import com.tunjid.heron.ui.text.Memo
 import heron.ui.core.generated.resources.Res
+import heron.ui.core.generated.resources.error_atproto_with_error
 import heron.ui.core.generated.resources.error_expired_session
 import heron.ui.core.generated.resources.error_generic
+import heron.ui.core.generated.resources.error_invalid_token
 import heron.ui.core.generated.resources.error_muted_thread
+import heron.ui.core.generated.resources.error_network_bad_request
+import heron.ui.core.generated.resources.error_network_forbidden
+import heron.ui.core.generated.resources.error_network_not_found
+import heron.ui.core.generated.resources.error_network_server_error
+import heron.ui.core.generated.resources.error_network_too_many_requests
+import heron.ui.core.generated.resources.error_network_unauthorized
+import heron.ui.core.generated.resources.error_network_unknown
 import heron.ui.core.generated.resources.error_notification_filtered_out
 import heron.ui.core.generated.resources.error_record_creation
 import heron.ui.core.generated.resources.error_restricted_profile
@@ -50,14 +61,30 @@ val HeronException.messageResource: Memo.Resource
         is UnknownNotificationException -> Memo.Resource(stringResource = Res.string.error_unknown_notification)
         is UnresolvableProfileException -> Memo.Resource(stringResource = Res.string.error_unresolvable_profile)
         is UnresolvableRecordException -> Memo.Resource(stringResource = Res.string.error_unresolvable_record)
+        is InvalidTokenException -> Memo.Resource(stringResource = Res.string.error_invalid_token)
+        is AtProtoException ->
+            if (error != null) Memo.Resource(
+                stringResource = Res.string.error_atproto_with_error,
+                args = listOf(error as Any),
+            )
+            else when (statusCode) {
+                400 -> Memo.Resource(stringResource = Res.string.error_network_bad_request)
+                401 -> Memo.Resource(stringResource = Res.string.error_network_unauthorized)
+                403 -> Memo.Resource(stringResource = Res.string.error_network_forbidden)
+                404 -> Memo.Resource(stringResource = Res.string.error_network_not_found)
+                429 -> Memo.Resource(stringResource = Res.string.error_network_too_many_requests)
+                in 500..599 -> Memo.Resource(stringResource = Res.string.error_network_server_error)
+                else -> Memo.Resource(
+                    stringResource = Res.string.error_network_unknown,
+                    args = listOf(statusCode as Any),
+                )
+            }
     }
 
 val Throwable.userMessage: Memo
-    get() = if (this is HeronException) {
-        this.messageResource
-    } else {
-        Memo.Resource(
+    get() =
+        if (this is HeronException) this.messageResource
+        else Memo.Resource(
             stringResource = Res.string.error_generic,
-            args = listOf(message ?: toString()),
+            args = listOf((message ?: toString()) as Any),
         )
-    }
