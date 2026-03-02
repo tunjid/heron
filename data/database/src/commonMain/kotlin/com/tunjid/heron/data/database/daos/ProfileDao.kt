3363cafb09fca88634614665d195c9f57833422e
joelmuraguri <joelkmuraguri21@gmail.com>
2026-03-02 18:20:28 +0300

    update ProfileStatusUpdate repository impl

--- data/database/src/commonMain/kotlin/com/tunjid/heron/data/database/daos/ProfileDao.kt
+++ data/database/src/commonMain/kotlin/com/tunjid/heron/data/database/daos/ProfileDao.kt
@@ -26,13 +26,14 @@ import androidx.room.Upsert
 import com.tunjid.heron.data.core.types.BlockUri
 import com.tunjid.heron.data.core.types.FollowUri
 import com.tunjid.heron.data.core.types.Id
+import com.tunjid.heron.data.core.types.ImageUri
 import com.tunjid.heron.data.core.types.ProfileId
 import com.tunjid.heron.data.database.entities.PopulatedProfileEntity
 import com.tunjid.heron.data.database.entities.ProfileEntity
-import com.tunjid.heron.data.database.entities.ProfileStatusEntity
 import com.tunjid.heron.data.database.entities.partial
 import com.tunjid.heron.data.database.entities.profile.ProfileViewerStateEntity
 import com.tunjid.heron.data.database.entities.profile.partial
+import kotlin.time.Instant
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -153,6 +154,34 @@ interface ProfileDao {
 
     @Query(
         """
+        UPDATE profiles SET
+            live_uri = :uri,
+            live_value = :value,
+            live_uriLink = :uriLink,
+            live_title = :title,
+            live_description = :description,
+            live_thumbnail = :thumbnail,
+            live_expiresAt = :expiresAt,
+            live_active = :active,
+            live_disabled = :disabled
+        WHERE did = :did
+    """,
+    )
+    suspend fun updateStatus(
+        did: ProfileId,
+        uri: String?,
+        value: String?,
+        uriLink: String?,
+        title: String?,
+        description: String?,
+        thumbnail: ImageUri?,
+        expiresAt: Instant?,
+        active: Boolean?,
+        disabled: Boolean?,
+    )
+
+    @Query(
+        """
         UPDATE profileViewerStates
         SET `following` = NULL
         WHERE `following` = :uri
@@ -171,18 +200,5 @@ interface ProfileDao {
     )
     suspend fun deleteBlock(
         uri: BlockUri,
-    )
-
-    @Upsert
-    suspend fun upsertStatus(
-        entity: ProfileStatusEntity
-    )
-
-    @Query("""
-        DELETE FROM profile_statuses
-        WHERE profileId = :profileId
-    """)
-    suspend fun deleteStatus(
-        profileId: ProfileId
     )
 }
