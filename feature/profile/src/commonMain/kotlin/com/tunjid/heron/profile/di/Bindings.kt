af4316fe692793a9c48982aaf36b586e901bb673
joelmuraguri <joelkmuraguri21@gmail.com>
2026-04-06 15:52:50 +0300

    use wrapContentWidth on profile screen back button to prevent clipping

--- feature/profile/src/commonMain/kotlin/com/tunjid/heron/profile/di/Bindings.kt
+++ feature/profile/src/commonMain/kotlin/com/tunjid/heron/profile/di/Bindings.kt
@@ -19,6 +19,7 @@ package com.tunjid.heron.profile.di
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -183,8 +184,7 @@ class ProfileBindings(
                 showNavigation = true,
                 topBar = {
                     PoppableDestinationTopAppBar(
-                        // Limit width so tabs may be tapped
-                        modifier = Modifier.width(72.dp),
+                        modifier = Modifier.wrapContentWidth(),
                         transparencyFactor = ::fullAppbarTransparency,
                         onBackPressed = { viewModel.accept(Action.Navigate.Pop) },
                     )
