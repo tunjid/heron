0a5ba62685d1b28017132121997cac0c67df4c4b
joelmuraguri <joelkmuraguri21@gmail.com>
2026-04-06 16:22:04 +0300

    use wrapContentWidth(Alignment.Start) on app bar and remove unused imports

--- feature/profile/src/commonMain/kotlin/com/tunjid/heron/profile/di/Bindings.kt
+++ feature/profile/src/commonMain/kotlin/com/tunjid/heron/profile/di/Bindings.kt
@@ -18,12 +18,11 @@ package com.tunjid.heron.profile.di
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.round
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -184,7 +183,8 @@ class ProfileBindings(
                 showNavigation = true,
                 topBar = {
                     PoppableDestinationTopAppBar(
-                        modifier = Modifier.wrapContentWidth(),
+                        // Limit width so tabs may be tapped
+                        modifier = Modifier.wrapContentWidth(Alignment.Start),
                         transparencyFactor = ::fullAppbarTransparency,
                         onBackPressed = { viewModel.accept(Action.Navigate.Pop) },
                     )
