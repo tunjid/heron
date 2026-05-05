16ebee5cb382cae306ae6871999deb10598f06f3
joelmuraguri <joelkmuraguri21@gmail.com>
2026-05-05 12:02:15 +0300

    define models

---
+++ data/models/src/commonMain/kotlin/com/tunjid/heron/data/core/models/Artist.kt
@@ -0,0 +1,24 @@
+package com.tunjid.heron.data.core.models
+
+import com.tunjid.heron.data.core.types.ArtistId
+import com.tunjid.heron.data.core.types.ArtistUri
+import com.tunjid.heron.data.core.types.ImageUri
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Artist(
+    val id: ArtistId,
+    val name: String,
+    val picture: ImageUri?,
+    val uri: ArtistUri,
+    val playCount: Long? = null,
+    val uniqueListeners: Long? = null,
+    val tags: List<String>? = null,
+) : Record {
+
+    override val reference: Record.Reference =
+        Record.Reference(
+            id = id,
+            uri = uri,
+        )
+}
