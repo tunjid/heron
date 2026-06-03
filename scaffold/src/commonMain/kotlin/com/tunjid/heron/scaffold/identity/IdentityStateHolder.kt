14f6c737b1d3bf8371d9ccc4de5c32af1c170828
Adetunji Dahunsi <tjdah100@gmail.com>
2026-06-03 16:26:59 +0100

    Organize app state holder classes and types better

--- scaffold/src/commonMain/kotlin/com/tunjid/heron/scaffold/identity/IdentityStateHolder.kt
+++ scaffold/src/commonMain/kotlin/com/tunjid/heron/scaffold/identity/IdentityStateHolder.kt
@@ -1,9 +1,5 @@
 package com.tunjid.heron.scaffold.identity
 
-import androidx.compose.runtime.Stable
-import com.tunjid.heron.data.core.models.Preferences
-import com.tunjid.heron.data.core.models.Profile
-import com.tunjid.heron.data.core.models.SessionSummary
 import com.tunjid.heron.data.core.utilities.Outcome
 import com.tunjid.heron.data.di.AppMainScope
 import com.tunjid.heron.data.repository.AuthRepository
@@ -17,12 +13,8 @@ import com.tunjid.heron.ui.text.Memo
 import com.tunjid.mutator.coroutines.ActionSuspendingStateMutator
 import com.tunjid.mutator.coroutines.actionSuspendingStateMutator
 import com.tunjid.mutator.coroutines.launchMutationsIn
-import com.tunjid.snapshottable.SnapshotSpec
-import com.tunjid.snapshottable.Snapshottable
 import dev.zacsweers.metro.Inject
 import heron.ui.core.generated.resources.error_session_switch
-import kotlin.collections.List
-import kotlin.collections.emptyList
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.CoroutineScope
@@ -33,53 +25,6 @@ import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
 
 interface IdentityStateHolder : ActionSuspendingStateMutator<IdentityAction, IdentityState>
-
-sealed class IdentityAction(
-    val key: String,
-) {
-    sealed class Switch :
-        IdentityAction(
-            key = "switch",
-        ) {
-        data object Cancel : Switch()
-        data object Choose : Switch()
-        data class Transition(
-            val summary: SessionSummary,
-        ) : Switch()
-    }
-}
-
-@Stable
-@Snapshottable
-interface IdentityState {
-    sealed class SwitchStatus {
-        sealed class Stable : SwitchStatus() {
-            data object Idle : SwitchStatus.Stable()
-            data class Error(
-                val memo: Memo,
-            ) : SwitchStatus.Stable()
-        }
-
-        data object Choosing : SwitchStatus()
-        data class Switching(
-            val session: SessionSummary,
-        ) : SwitchStatus()
-    }
-
-    @SnapshotSpec
-    data class Immutable(
-        val signedInProfile: Profile? = null,
-        val preferences: Preferences? = null,
-        val switchStatus: SwitchStatus = SwitchStatus.Stable.Idle,
-        val pastSessions: List<SessionSummary> = emptyList(),
-    ) : IdentityState
-}
-
-internal val IdentityState.isSignedIn
-    get() = signedInProfile != null
-
-internal val IdentityState.isStable
-    get() = switchStatus is IdentityState.SwitchStatus.Stable
 
 @Inject
 class AppIdentityStateHolder(
