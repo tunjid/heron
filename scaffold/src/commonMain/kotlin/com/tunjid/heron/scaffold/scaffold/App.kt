9218ddd1f6ba7d7ddc80e3b31dfe806d4070cbdd
Adetunji Dahunsi <tjdah100@gmail.com>
2026-06-18 23:33:56 -0400

    Clean up imports

--- scaffold/src/commonMain/kotlin/com/tunjid/heron/scaffold/scaffold/App.kt
+++ scaffold/src/commonMain/kotlin/com/tunjid/heron/scaffold/scaffold/App.kt
@@ -24,7 +24,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
