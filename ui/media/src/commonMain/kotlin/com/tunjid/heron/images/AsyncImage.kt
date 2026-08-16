beb8fd53f997d778ce6b5565849046091ad0e8bb
Adetunji Dahunsi <tjdah100@gmail.com>
2026-08-16 15:35:36 -0400

    Streamline image size buckets

--- ui/media/src/commonMain/kotlin/com/tunjid/heron/images/AsyncImage.kt
+++ ui/media/src/commonMain/kotlin/com/tunjid/heron/images/AsyncImage.kt
@@ -348,8 +348,8 @@ private fun Int.roundedUpToDecodeBucket(): Int {
 }
 
 private val DecodeBucketsPx = intArrayOf(
-    64, 96, 128, 256, 384, 640, 750,
-    828, 1080, 1200, 1920, 2048, 3840,
+    96, 128, 256, 320, 480, 640,
+    720, 1024, 1280, 1536, 1920,
 )
 
 private val ImageInterpolationSpec = spring<Float>(
